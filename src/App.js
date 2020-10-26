d4257ffd7f8acea0a157048a7c94de7a7785fabb
stanislavlebedyantsev <stanislav.lebedyantsev@gmail.com>
2020-10-26 19:08:22 +0300

    Add route for Sign in button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from "./components/Header/Header";
+import {Route, BrowserRouter} from "react-router-dom";
+import LoginComponent from "./components/Login/LoginComponent";
+import Registration from "./components/Registration/Registration";
+import Mainpage from "./components/MainPage/Mainpage";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <div>
+          <Route path={"/login"} component={LoginComponent}/>
+          <Route path={"/registration"} component={Registration}/>
+          <Route path={"/"} component={Mainpage}/>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
