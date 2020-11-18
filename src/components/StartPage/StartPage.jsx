2828fbcccc68e4dfa0974538f46b27aeb62caedc
stanislavlebedyantsev <stanislav.lebedyantsev@gmail.com>
2020-11-18 17:34:01 +0300

    Added description on start page

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -13,6 +13,9 @@ const StartPage = (props) => {
           </div>
           <div className={s.projectVision}>
             <p>Your way to be better</p>
+            <p>Choice Manager is a site that helps students making choices based on passion, skills and an open mind.
+              The site presents a structured way, in which you will discover new things about yourself and making it
+              clear where you want to take action. </p>
           </div>
           <div className={s.aboutUs}>
             <NavLink to="/about" className={s.aboutUsLink}>Learn more</NavLink>
