6492bee0d5657925ec311d22579ca05322275f47
ArtemKomarec <artemkomarec10@gmail.com>
2020-11-24 21:10:36 +0300

    Changes: Start page, slider

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -7,14 +7,22 @@ const StartPage = (props) => {
   return (
     <div>
       <ul className={s.body_slides}>
-        <li>
-          <div></div>
-        </li>
-        <li>
-          <div></div>
-        </li>
-        <li>
-        </li>
+          <li>
+              <div className={s.slider1}>
+                  <div className={s.slider1H1}>
+                      Choice manager.<br/> The way to create your own
+                  </div>
+                  <div className={s.slider1Right}>
+                      <p>One of the most important things in life is to find your life vision and decide what you strive at and what you want to accomplish.
+                      <br/><br/>    Otherwise you drift from place to place without a plan, without a purpose. A life based on random decisions is a confusing and often difficult one. </p>
+                  </div>
+              </div>
+          </li>
+          <li>
+              <div>asd</div>
+          </li>
+          <li>
+          </li>
       </ul>
       <div>
         <About/>
