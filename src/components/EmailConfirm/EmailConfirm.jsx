cead2625c5646a6690c3c111b2ecb5fa878d9732
ArtemKomarec <artemkomarec10@gmail.com>
2020-12-05 11:24:28 +0300

    Changes: Activation page

--- src/components/EmailConfirm/EmailConfirm.jsx
+++ src/components/EmailConfirm/EmailConfirm.jsx
@@ -3,14 +3,23 @@ import {NavLink} from "react-router-dom";
 
 const EmailConfirm = () => {
   return (
-    <div>
-      <div className={s.container}>
-        <div className={s.headName}><p>Thank you for using Choice-manager</p></div>
-        <div className={s.sendAgain}>
-          <p>Your email successful confirmed</p>
-          <p>Now you can <NavLink to={'/login'}>Sign in</NavLink></p>
+    <div className={s.background}>
+        <div className={s.container}>
+            <div className={s.logoGrid}>
+                <div className={s.d3}>
+                </div>
+                <p className={s.chcMngr}>Choice Manager</p>
+            </div>
+            <div className={s.welcomeGrid}>
+                <p className={s.H1}>Welcome!</p>
+                <p className={s.underWelc}>We are thrilled to have you on board</p>
+            </div>
+            <div className={s.thirdGrid}>
+                <p className={s.introP}>With your new Choice Manager account you can create your goal to bit it.Choice Manager is a site that helps students making choices based on passion, skills and an open mind. The site presents a structured way, in which you will discover new things about yourself and making it clear where you want to take action.
+                </p>
+                <button className={s.btn}>Start working</button>
+            </div>
         </div>
-      </div>
     </div>
   );
 };
